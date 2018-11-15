c206bb0b406c5e38dadbcf249864f77d79113b83
Andre <andre@MacBookPro-Andrei.local>
2018-11-15 08:59:37 -0800

    init

---
+++ run-playbook.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ansible-playbook -i hosts.ini aws-docker-vpn.yml
---
+++ vpn/files/vpn-status.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker exec -it ipsec-vpn-server ipsec status
+
+docker exec -it ipsec-vpn-server ipsec whack --trafficstatus
